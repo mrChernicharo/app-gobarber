f4535b523a11f0aed04fb16de6d893ac40a7812f
mrChernicharo <felipe.chernicharo@gmail.com>
2020-05-02 00:04:33 -0300

    bg + statusBar

--- appgobarber/src/App.tsx
+++ appgobarber/src/App.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StatusBar, Text } from 'react-native';
 
 const App: React.FC = () => (
-  <View>
-    <Text>Hello World</Text>
-  </View>
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <View
+      style={{
+        flex: 1,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#312e38',
+      }}
+    >
+      <Text
+        style={{
+          color: '#f3f3f3',
+          fontWeight: 'bold',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        Hello World
+      </Text>
+    </View>
+  </>
 );
 
 export default App;
