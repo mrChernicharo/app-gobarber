9595cab7a28bb6e2307f709a671d8a4889f07c09
mrChernicharo <felipe.chernicharo@gmail.com>
2020-05-03 19:23:42 -0300

    hook useAuth | SignIn funcionando

--- appgobarber/src/pages/SignIn/index.tsx
+++ appgobarber/src/pages/SignIn/index.tsx
@@ -10,11 +10,12 @@ import {
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
+import * as Yup from 'yup';
 
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 
-import * as Yup from 'yup';
+import { useAuth } from '../../hooks/auth';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
@@ -30,53 +31,54 @@ import {
   CreateAccountButtonText,
 } from './styles';
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const passwordInputRef = useRef<TextInput>(null);
   const navigation = useNavigation();
 
-  interface SignInFormData {
-    email: string;
-    password: string;
-  }
+  const { signIn, user } = useAuth();
 
-  const handleSignIn = useCallback(async (data: SignInFormData) => {
-    try {
-      formRef.current?.setErrors({});
+  console.log(user);
 
-      const schema = Yup.object().shape({
-        email: Yup.string()
-          .required('Email obrigatório')
-          .email('Digite email válido'),
-        password: Yup.string().min(6, 'Senha obrigatória'),
-      });
+  const handleSignIn = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
 
-      await schema.validate(data, {
-        abortEarly: false,
-      });
-      // await signIn({
-      //   email: data.email,
-      //   password: data.password,
-      // });
-    } catch (err) {
-      if (err instanceof Yup.ValidationError) {
-        const errors = getValidationErrors(err);
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('Email obrigatório')
+            .email('Digite email válido'),
+          password: Yup.string().min(6, 'Senha obrigatória'),
+        });
 
-        formRef.current?.setErrors(errors);
-        return; // <== para impedir os toasts
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+        await signIn({
+          email: data.email,
+          password: data.password,
+        });
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+          return; // <== para impedir os toasts
+        }
+        Alert.alert(
+          'Erro na autenticação',
+          'Erro ao fazer Login. Cheque as credenciais.',
+        );
       }
-      Alert.alert(
-        'Erro na autenticação',
-        'Erro ao fazer Login. Cheque as credenciais.',
-      );
-      // disparar toast
-      // addToast({
-      //   type: 'error',
-      //   title: 'Erro na autenticação',
-      //   description: 'Erro ao fazer Login. Cheque as credenciais.',
-      // });
-    }
-  }, []);
+    },
+    [signIn],
+  );
 
   return (
     <>
