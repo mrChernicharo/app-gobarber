f4bc18497756a7b57d038dbeb78caa6c23058727
mrChernicharo <felipe.chernicharo@gmail.com>
2020-06-15 18:25:57 -0300

    Create Appointment

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,9 +1,59 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { Container } from './style';
+import React, { useCallback, useState } from 'react';
+import { useRoute } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+
+import { useAuth } from '../../hooks/auth';
+
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar,
+} from './style';
+
+interface RouteParams {
+  provider_id: string;
+}
+
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
 
 const CreateAppontment: React.FC = () => {
-  return <Container />;
+  const route = useRoute();
+  const { user } = useAuth();
+  const { goBack } = useNavigation();
+  const { provider_id } = route.params as RouteParams;
+
+  const [providers, setProviders] = useState<Provider[]>([]);
+
+  const navigateBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
+
+  // useEffect(() => {
+  //   api.get('providers').then((response) => {
+  //     setProviders(response.data);
+  //   });
+  // }, []);
+
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={navigateBack}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+
+        <HeaderTitle>Cabeleireiros</HeaderTitle>
+
+        <UserAvatar source={{ uri: user.avatar_url }} />
+      </Header>
+    </Container>
+  );
 };
 
 export default CreateAppontment;
--- src/pages/CreateAppointment/style.ts
+++ src/pages/CreateAppointment/style.ts
@@ -1,5 +1,30 @@
 import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
+`;
+
+export const Header = styled.View`
+  padding: 24px;
+  padding-top: ${getStatusBarHeight()}px;
+  background: #28262e;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+export const BackButton = styled.TouchableOpacity``;
+
+export const HeaderTitle = styled.Text`
+  color: #f4ede8;
+  font-size: 20px;
+  font-family: 'RobotoSlab-Regular';
+  margin-left: 16px;
+`;
+export const UserAvatar = styled.Image`
+  width: 56px;
+  height: 56px;
+  border-radius: 28px;
+  margin-left: auto;
 `;
